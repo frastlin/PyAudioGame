1cf48d5d37c5b63203dbb3d08c60a83b495a57db
Brandon Biggs <brandonkeithbiggs@gmail.com>
2018-12-18 02:59:13 -0800

    Added a keymap and its tests

--- pyaudiogame/keymap.py
+++ pyaudiogame/keymap.py
@@ -2,33 +2,39 @@
 Manages the usage of keymaps
 usage:
 keymap = KeyMap()
-keymap.add(key=["f"], mods=["shift"], event="fligh)
+keymap.add(keys=["f"], mods=["shift"], event="fligh)
 """
 
 class KeyMap(object):
-	def __init__(self, keymap):
+	def __init__(self, keymap=[]):
 		self._keymap = []
 
 		for mapping in keymap:
 			self.add(mapping)
 
-	def add(self, mapping={}, key=[], mods=[], event=None, function=None):
+	def add(self, mapping={}, keys=[], mods=[], event=None, function=None):
 		"""adds a key mapping to self._keymap. One can either pass in a dict of the keywords, or use the arguments to create the mapping. If one passes in both a dict and some keywords, note that the keywords will be over-ridden by the dict. either one or both of function or event need to be passed."""
 		local_mapping = locals()
-		# The next section keeps mapping from adding new keys, which is what it would do if one used update.
+		# The next section keeps mapping from adding new key, which is what it would do if one used update.
 		for k in local_mapping:
 			local_mapping[k] = mapping.get(k, local_mapping[k])
 		del local_mapping["mapping"] # so if it is empty in the next line, it doesn't trigger as false. One could also make it equal True, but this is more explicit
-		if local_mapping["key"] and (local_mapping["function"] or local_mapping["event"]):
-			# make sure key and mods are lists
-			local_mapping["key"] = list(local_mapping["key"])
-			local_mapping["mods"] = list(local_mapping["mods"])
+		if local_mapping["keys"] and (local_mapping["function"] or local_mapping["event"]):
+			# make sure key and mods are sets (not lists because we check if set == set in the getEvent function and list == list doesn't work if the items are out of order)
+			local_mapping["keys"] = set(list(local_mapping["keys"]))
+			local_mapping["mods"] = set(list(local_mapping["mods"]))
 			self._keymap.append(local_mapping)
 		else:
 			missing_items = [k for k in local_mapping if not local_mapping[k] and k != "mods"]
 			missing_items = ', '.join(missing_items)
 			raise Exception("KeymapError: KeyMap.add was passed a mapping without a value for: %s" % missing_items)
 
-	def getEvent(self, keys, mods):
-		"""Gets the event that is triggered by the list of keys and mods"""
-		return True
+	def getEvent(self, keys, mods=[]):
+		"""Gets the event that is triggered by the list of key and mods."""
+		keys = set(list(keys))
+		mods = set(list(mods))
+		for mapping in self._keymap:
+			if mapping['keys'] == keys and mapping['mods'] == mods:
+				if mapping['function']:
+					return mapping['function']()
+				return mapping['event']
--- tests/test_keymap.py
+++ tests/test_keymap.py
@@ -1,5 +1,29 @@
+import unittest
 from pyaudiogame.keymap import KeyMap
 
-def test_add_function():
-	keymap = KeyMap()
-	keymap.add(key=["f"], mods=["shift"], event="fligh")
+def funcCallback():
+	return True
+
+class TestKeymap(unittest.TestCase):
+	def test_add_function(self):
+		keymap = KeyMap()
+		keymap.add(keys=["f"], mods=["shift"], event="fligh")
+		keymap.add({
+			"keys": "j",
+			"event": "jump"
+		})
+		self.assertEqual(keymap.getEvent(keys=["f"], mods=["shift"]), "fligh")
+		self.assertEqual(keymap.getEvent(keys="j"), "jump")
+		self.assertEqual(keymap.getEvent("space"), None)
+
+	def test_add_in_object(self):
+		keymap = KeyMap([
+			{'keys':'space', 'event': "oranges"},
+			{'keys':['f', 'a'], 'mods': ['ctrl','shift'], 'function': funcCallback},
+		])
+		self.assertEqual(keymap.getEvent(keys=['f', 'a'], mods=['ctrl','shift']), funcCallback())
+		self.assertEqual(keymap.getEvent(keys='space'), 'oranges')
+
+
+if __name__ == '__main__':
+	unittest.main
