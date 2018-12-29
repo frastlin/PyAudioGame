6d540172a3e3fd9d27572b4cf992103d5a8b45a0
Brandon Biggs <brandonkeithbiggs@gmail.com>
2018-12-28 17:53:45 -0800

    Added the add_polygon to the grid class

--- pyaudiogame/ui/grid.py
+++ pyaudiogame/ui/grid.py
@@ -20,11 +20,20 @@ class Grid(object):
 		self.add_wall(width, width, 0, height)
 
 	def check(self, x, y):
-		"""Call this to see if there is an object"""
+		"""Checks to see if the passed point is in any of the specified polys"""
+		point_in_polygon = self.point_in_polygon
+		for p in self.objects:
+			if point_in_polygon(x, y, p.poly):
+				if p.callback:
+					return p.callback()
+				return True
+
+	def check_rect(self, x, y):
+		"""Checks if a point is in a rect"""
 		for o in self.objects:
 			if x >= o.min_x and x <= o.max_x and y >= o.min_y and y <= o.max_y:
-				if o.run:
-					return o.run()
+				if o.callback:
+					return o.callback()
 				return True
 
 	def point_in_polygon(self, x, y, poly):
@@ -37,7 +46,9 @@ class Grid(object):
 			return None
 		for i in range(n+1):
 			p2x, p2y = poly[i % n]
-			if x == p2x and y == p2y:
+			if len(poly) == 2 and x >= min(p1x,p2x) and x <= max(p1x,p2x) and y >= min(p1y,p2y) and y <= max(p1y,p2y): # it is a single line
+				return True
+			if x == p2x and y == p2y: #it is on a point
 				return True
 			if y > min(p1y, p2y):
 				if y <= max(p1y,p2y):
@@ -45,28 +56,53 @@ class Grid(object):
 						if p1y != p2y:
 							xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
 						if p1x == p2x or x <= xinters:
+							print("ran: ", ([p1x, p1y], [p2x, p2y]))
 							inside = not inside
 			p1x, p1y = p2x, p2y
 		return inside
 
-
-	def add_wall(self, min_x, max_x, min_y, max_y, run=None):
+	def add_wall(self, min_x, max_x, min_y, max_y, callback=None):
 		"""Adds a Wall object to the object list"""
-		new_wall = Wall(min_x, max_x, min_y, max_y, run)
+		new_wall = Wall(min_x, max_x, min_y, max_y, callback)
 		self.objects.append(new_wall)
 		return new_wall
 
+	def add_polygon(self, poly, callback=None):
+		"""Creates a Polygon and adds it to the objects list"""
+		p = Polygon(poly, callback)
+		self.objects.append(p)
+		return p
+
 class Wall(object):
 	"""This just has the properties for basic Wall objects"""
-	def __init__(self, min_x, max_x, min_y, max_y, run=None):
+	def __init__(self, min_x, max_x, min_y, max_y, callback=None):
 		self.min_x = min_x
 		self.max_x = max_x
 		self.min_y = min_y
 		self.max_y = max_y
-		self.poly = [[min_x, min_y], [min_x, max_y], [max_x, max_y], [max_x, min_y]]
-		self.run = run
+		self.poly = [(min_x, min_y), (min_x, max_y), (max_x, max_y), (max_x, min_y)]
+		self.callback = callback
+		poly_set = list(set(self.poly))
+		if len(poly_set) == 2:
+			self.poly = poly_set
 
 	def __repr__(self):
 		"""Call this class to see the below returned"""
 		return "WallObject:x(%d, %d)y(%d, %d)" % (self.min_x, self.max_x, self.min_y, self.max_y)
 
+class Polygon(object):
+	def __init__(self, poly, callback=None):
+		self.poly = poly
+		self.callback = None
+
+	def __repr__(self):
+		return self.poly
+
+if __name__ == '__main__':
+	my_grid = Grid(50, 50)
+	o = my_grid.objects[3]
+	x, y = (50, 1)
+	print(o.poly)
+	print(x >= o.min_x and x <= o.max_x and y >= o.min_y and y <= o.max_y)
+	print(my_grid.point_in_polygon(x, y, o.poly))
+	print(my_grid.point_in_polygon(x, y, [[50, 0], [50, 50]]))
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -8,13 +8,17 @@ class TestGrid(unittest.TestCase):
 		dumb_wall = grid.Wall(1,10, 0,1)
 		self.assertEqual(dumb_wall.max_y, 1)
 		self.assertEqual(dumb_wall.min_x, 1)
-		self.assertEqual(dumb_wall.poly, [[1,0], [1,1], [10, 1], [10, 0]])
+		self.assertEqual(dumb_wall.poly, [(1,0), (1,1), (10, 1), (10, 0)])
 
 	def test_sides(self):
+		self.assertEqual(my_grid.check_rect(50, 1), True)
 		self.assertEqual(my_grid.check(50, 1), True)
+		self.assertEqual(my_grid.check_rect(5,50), True)
 		self.assertEqual(my_grid.check(5,50), True)
+		self.assertEqual(my_grid.check_rect(5,5), None)
 		self.assertEqual(my_grid.check(5,5), None)
 		#This is because the sides of the whole thing are just 1 square wide.
+		self.assertEqual(my_grid.check_rect(51,1), None)
 		self.assertEqual(my_grid.check(51,1), None)
 
 	def test_adding_areas(self):
@@ -23,17 +27,22 @@ class TestGrid(unittest.TestCase):
 		my_grid.add_wall(4,4, 3,6)
 		my_grid.add_wall(6,6, 3,4)
 		#check inside the box
+		self.assertEqual(my_grid.check_rect(5,5), None)
 		self.assertEqual(my_grid.check(5,5), None)
 		#test the wall
+		self.assertEqual(my_grid.check_rect(5,6), True)
 		self.assertEqual(my_grid.check(5,6), True)
 		#test the door
+		self.assertEqual(my_grid.check_rect(6,5), None)
 		self.assertEqual(my_grid.check(6,5), None)
 
-	def test_zones_tiles_without_substance(self):
-		"""Test that you can make walls that you can walk through"""
+	def test_callback_function(self):
+		"""Test that you can make walls that you can walk through and the callback is run"""
 		my_grid.add_wall(7,9, 2,5, lambda:None)
 		my_grid.add_wall(20,25, 2,5, lambda:"pizza")
+		self.assertEqual(my_grid.check_rect(7,3), None)
 		self.assertEqual(my_grid.check(7,3), None)
+		self.assertEqual(my_grid.check_rect(22,4), "pizza")
 		self.assertEqual(my_grid.check(22,4), "pizza")
 
 	def test_point_in_polygon(self):
@@ -52,3 +61,12 @@ class TestGrid(unittest.TestCase):
 		# tests the wall poly
 		wall = grid.Wall(0,10, 0,1)
 		self.assertEqual(point_in_polygon(None, 4, 1, wall.poly), True)
+		# test a line
+		line = [[50, 0], [50, 50]]
+		self.assertEqual(point_in_polygon(None, 50, 1, line), True)
+
+	def test_create_poly(self):
+		points = [(2,2), (2, 10), (12, 10), (12, 2)]
+		my_grid = grid.Grid(50, 50)
+		my_grid.add_polygon(points)
+		self.assertEqual(my_grid.check(4,4), True)
