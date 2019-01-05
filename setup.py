ac7c2cfefaf393732994e1ec65b8c88d04e0b73c
Brandon Biggs <brandonkeithbiggs@gmail.com>
2019-01-04 21:39:50 -0800

    added a script to install pywin32 to work with accessible_output2. Also added the setuptools.find_packages function to remove the need to add every new dir that is created in the pyaudiogame folder.

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-try:
-	from setuptools import setup
-except ImportError:
-	from distutils.core import setup
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+
+
+class PostDevelopCommand(develop):
+	"""Post-installation for development mode."""
+	def run(self):
+		check_call("pip install pywin32")
+		develop.run(self)
+
+class PostInstallCommand(install):
+	"""Post-installation for installation mode."""
+	def run(self):
+		check_call("pip install pywin32")
+		install.run(self)
 
 config = {
 	'description': 'PyAudioGame is a toolkit for making audio games in python.',
@@ -9,18 +22,18 @@ config = {
 	'url': 'https://github.com/frastlin/PyAudioGame',
 	'download_url': 'https://codeload.github.com/frastlin/PyAudioGame/zip/master',
 	'author_email': 'brandonkeithbiggs@gmail.com',
-	'version': '0.01a',
+	'version': '0.01',
 	'install_requires': ['pygame', 'inputs', 'accessible_output2'],
-	'dependency_links': ['https://github.com/frastlin/accessible_output2/tarball/master#egg=accessible_output2-0.12'],
+	'dependency_links': ['https://github.com/frastlin/accessible_output2/tarball/master#egg=accessible_output2-0.12', 'https://github.com/frastlin/inputs/tarball/master#egg=inputs-0.5'],
 #	'dependency_links': ['git+https://github.com/frastlin/accessible_output2.git@master'],
 
-	#accessible_output2 is broken and accessible_output is in packages.
-	#can't get the links to an hg repo to work for some reason...
-#	dependency_links=['hg+http://hg.q-continuum.net/accessible_output2/'],
-	#Added the accessible_output to the packages for now, will take it out when the above work with accessible_output2
-	'packages': ['pyaudiogame', 'pyaudiogame/ui', 'pyaudiogame/templates', 'pyaudiogame/helper', 'requires/accessible_output', 'requires/libloader/libloader', 'requires/platform_utils/platform_utils'],
+	'packages': find_packages(),
 	'scripts': [],
-	'name': 'pyaudiogame'
-	}
+	'name': 'pyaudiogame',
+	'cmdclass': {
+		'develop': PostDevelopCommand,
+		'install': PostInstallCommand,
+	},
+}
 
 setup(include_package_data=True, **config)
